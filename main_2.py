bb40bbf0bbae4121c136715609be3687e8f4b57e
Galactica91 <osheva91@yandex.com>
2025-01-18 11:59:28 +0300

    Update main_2.py

--- main_2.py
+++ main_2.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, Path
+from typing import Annotated
+
 app = FastAPI()
 
 @app.get("/")
@@ -14,6 +16,6 @@ async def user_id_page(user_id: int=Path(ge=1, le=100, description="Enter User I
     return {"message": f"Вы вошли как пользователь N {user_id}"}
 
 @app.get("/user/{username}/{age}")
-async def user_info(username: str=Path(min_length=5, max_length=20, description="Enter your username", example="universe"),
-                    age: int=Path(le=120, ge=18, description="Enter your age", example="35")) -> dict:
-    return {"message": f"Информация о пользователе: имя - {username}, возраст - {age}"}
+async def user(username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser')],
+              age: Annotated[int, Path(ge=18, le=120, description='Enter age', example='24')]) -> str:
+  return f'Информация о пользователе. Имя: {username}, Возраст: {age}'
